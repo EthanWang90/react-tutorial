2a039b2429470e478a1a04894a9c62d704a5a249
Ethan Wang <ethanwang9011@gmail.com>
2018-07-29 22:41:16 +1200

    Board(parent) stores the states instead of Squares(children), children call the function defined in parent to react the events

--- src/Board.js
+++ src/Board.js
@@ -1,26 +1,52 @@
 import React, {Component} from "react";
 
 class Board extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            player: "X"
+        }
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.player;
+        this.setState({
+            squares: squares,
+        });
+        if(this.state.player === "X"){
+            this.setState({player: "O"})
+        }
+        else{
+            this.setState({player: "X"})
+        }
+    }
     renderSquare(i){
-        return <Square value={i}/>
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
     }
     render(){
+        const status = "next player: X";
         return(
             <div>
-                <div className="row1">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>                
-                <div className="row2">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>                
-                <div className="row3">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                <div>
+                    {status}
+                </div>
+                <div>
+                    <div className="row1">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>                
+                    <div className="row2">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>                
+                    <div className="row3">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
                 </div>
             </div>
         );
@@ -29,18 +55,18 @@ class Board extends Component{
 
 class Square extends Component{
 
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    };
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // };
     // if I want define a method using this, I must wrap method definition into one life cycle method
     
     // componentDidMount(){
     //     let currentComponent = this; // "this" doesn't go inside methods which are not life cycle method such as render() or componentDidMount()
         
-    //     clickChange(){
+    //     function(): clickChange(){
     //         console.log(1);
     //         console.log(this);
     //         currentComponent.setState({
@@ -63,8 +89,8 @@ class Square extends Component{
     render(){
         console.log(this);
         return(
-            <button className="square" onClick={() => this.setState({value: "X"})}> 
-                {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}> 
+                {this.props.value}
             </button>
         )
     }
