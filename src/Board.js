f15dead42d7236a2aacf1bf544b387d3863bf292
Ethan Wang <ethanwang9011@gmail.com>
2018-07-30 12:30:37 +1200

    re-define the Square component in a more simple way: functional component, removing the arrow function in the new component

--- src/Board.js
+++ src/Board.js
@@ -53,48 +53,56 @@ class Board extends Component{
     }
 }
 
-class Square extends Component{
-
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // };
-    // if I want define a method using this, I must wrap method definition into one life cycle method
-    
-    // componentDidMount(){
-    //     let currentComponent = this; // "this" doesn't go inside methods which are not life cycle method such as render() or componentDidMount()
-        
-    //     function(): clickChange(){
-    //         console.log(1);
-    //         console.log(this);
-    //         currentComponent.setState({
-    //             value: "X",
-    //         });
-    //     };
-    // };
-
-    // clickChange(){
-    //     console.log(1);
-    //     console.log(this);
-    //     let currentComponent = this; // "this" doesn't go inside methods which are not life cycle method such as render() or componentDidMount()
-    //     currentComponent.setState({
-    //         value: "X",
-    //     });
-    // }
-    // componentWillUpdate(){
-    //     console.log(this);
-    // };
-    render(){
-        console.log(this);
-        return(
-            <button className="square" onClick={() => this.props.onClick()}> 
-                {this.props.value}
-            </button>
-        )
-    }
-    //why this.setState works well in arrow function or? in render()
+function Square(props){ //Another component definition if this component only have render method
+    return( // Also, we don't need arrow function for the onClick event because in a class, we used an arrow function to access the correct this value, but in a functional component we don’t need to worry about this.
+        <button className = "square" onClick = {props.onClick}>
+            {props.value}
+        </button>
+    );
 }
+
+// class Square extends Component{
+
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state = {
+//     //         value: null,
+//     //     };
+//     // };
+//     // if I want define a method using this, I must wrap method definition into one life cycle method
+    
+//     // componentDidMount(){
+//     //     let currentComponent = this; // "this" doesn't go inside methods which are not life cycle method such as render() or componentDidMount()
+        
+//     //     function(): clickChange(){
+//     //         console.log(1);
+//     //         console.log(this);
+//     //         currentComponent.setState({
+//     //             value: "X",
+//     //         });
+//     //     };
+//     // };
+
+//     // clickChange(){
+//     //     console.log(1);
+//     //     console.log(this);
+//     //     let currentComponent = this; // "this" doesn't go inside methods which are not life cycle method such as render() or componentDidMount()
+//     //     currentComponent.setState({
+//     //         value: "X",
+//     //     });
+//     // }
+//     // componentWillUpdate(){
+//     //     console.log(this);
+//     // };
+//     render(){
+//         console.log(this);
+//         return(
+//             <button className="square" onClick={() => this.props.onClick()}> 
+//                 {this.props.value}
+//             </button>
+//         )
+//     }
+//     //why this.setState works well in arrow function or? in render()
+// }
 
 export default Board;
