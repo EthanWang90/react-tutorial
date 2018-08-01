2f3ab077298f8be1012f59f8a4c7bc3ba283da0b
Ethan Wang <ethanwang9011@gmail.com>
2018-08-01 13:33:03 +1200

    game function is finished without time travel, the calcWinner function position is different with official tutorial, I defined this calcWinner within the Board class.

--- src/Board.js
+++ src/Board.js
@@ -8,8 +8,29 @@ class Board extends Component{
             xIsNext: true,
         }
     }
+    calcWinner(squares){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++){
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a]===squares[b] && squares[b]===squares[c]){
+                return squares[a];
+            }
+        }
+    }
     handleClick(i){
         const squares = this.state.squares.slice();
+        if(squares[i] || this.calcWinner(squares)){
+            return
+        }
         squares[i] = this.state.xIsNext? "X": "O";
         this.setState({
             squares: squares,
@@ -20,7 +41,14 @@ class Board extends Component{
         return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
     }
     render(){
-        const status = "next player: X";
+        const winner = this.calcWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = "The winner is: " + winner;
+        }
+        else{
+            status = "next player: " + (this.state.xIsNext? "X":"O");
+        }
         return(
             <div>
                 <div>
@@ -55,6 +83,8 @@ function Square(props){ //Another component definition if this component only ha
         </button>
     );
 }
+
+
 
 // class Square extends Component{
 
