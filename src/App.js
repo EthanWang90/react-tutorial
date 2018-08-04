6222afc21a34092c6371af740569a3d97f179374
Ethan Wang <ethanwang9011@gmail.com>
2018-08-04 21:35:01 +1200

    show each step in history list.

--- src/App.js
+++ src/App.js
@@ -53,6 +53,17 @@ class App extends Component {
     const current = history[history.length - 1];
     const winner = this.calculateWinner(current.squares);
 
+    const moves = history.map((step, move)=>{
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -75,6 +86,7 @@ class App extends Component {
       </div>
       <div className="game-info">
         <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
       </div>
     );
